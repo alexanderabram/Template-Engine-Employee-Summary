0133fe06810944c5a1bbd09b877eb06bfbb52b1e
AMA <alexanderabram6440@gmail.com>
2020-06-18 18:28:43 -0500

    more intern.js edits.

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -4,17 +4,12 @@ class Intern extends Employee {
     constructor(name, id, email, school){
         super(name, id, email);
         this.school = school;   
-    };
-
-    //returns the school of the employee
+    }
     getSchool() {
         return this.school;
-    };
-
-    //returns the role of the employee
+    }
     getRole() {
         return "Intern";
-    };
-};
-//.exports allows this page to be used elsewhere
+    }
+}
 module.exports = Intern;
