438a59947ba8e9453339d91b7944e5be8628b4ce
AMA <alexanderabram6440@gmail.com>
2020-06-18 19:04:25 -0500

    working on app.js edits

--- app.js
+++ app.js
@@ -14,13 +14,10 @@ const render = require("./lib/htmlRenderer");
 async function start() {
     console.log("Let's make your Dream Team!");
 
-    // Set Variable to hold HTML
     let teamHTML = "";
 
-    // Variable to hold number of team members
     let teamSize;
 
-    // First Question to ask to set up loop
     await inquirer.prompt(
         {
             type: "number",
@@ -30,27 +27,21 @@ async function start() {
     )
         .then((data) => {
 
-            // Number of team members placed in teamSize for scope purposes.
-            // 1 is added start from 1 rather than 0 for user understanding.
             teamSize = data.noOfTeamMem + 1;
         });
 
-    // If Team Size is 0, will end program
     if (teamSize === 0) {
         console.log("I guess there is no one on your team...");
         return;
     }
 
-    // Loop begins to ask questions depending on the size of the team
     for (i = 1; i < teamSize; i++) {
 
-        // Global variables set
         let name;
         let id;
         let title;
         let email;
 
-        // Prompts user to answer the basic questions of the employee
         await inquirer.prompt([
             {
                 type: "input",
@@ -76,18 +67,15 @@ async function start() {
         ])
             .then((data) => {
 
-                // Takes data from user and places value in global variables
                 name = data.name;
                 id = data.id;
                 title = data.title;
                 email = data.email;
             });
 
-        // Switch Case depending on the title of the employee
         switch (title) {
             case "Manager":
 
-                // ask user of Manager's Office Number
                 await inquirer.prompt([
                     {
                         type: "input",
@@ -97,19 +85,14 @@ async function start() {
                 ])
                     .then((data) => {
 
-                        // Create a new object with all avaiable user input data
                         const manager = new Manager(name, id, email, data.officeNo);
 
-                        // Reads and places HTML from manager.html in teamMemever Variable
                         teamMember = fs.readFileSync("templates/manager.html");
 
-                        // Uses eval() to pass template literals from html files.
-                        // Adds the string to the team HTML.
                         teamHTML = teamHTML + "\n" + eval('`' + teamMember + '`');
                     });
                 break;
 
-            //Steps Similar to Manager but for intern
             case "Intern":
                 await inquirer.prompt([
                     {
@@ -125,7 +108,6 @@ async function start() {
                     });
                 break;
 
-            //Steps Similar to Manager but for engineer
             case "Engineer":
                 await inquirer.prompt([
                     {
@@ -141,17 +123,14 @@ async function start() {
                     });
                 break;
 
-        } // End of Switch Case
+        }
 
-    } // End of For loop
+    }
 
-    // Reads main.html and places html in a variable
     const mainHTML = fs.readFileSync("templates/main.html");
 
-    // Use eval to implement template literals in main.html and places teamHTML inside main template
     teamHTML = eval('`' + mainHTML + '`');
 
-    // write file to new team.html file
     fs.writeFile("output/team.html", teamHTML, function (err) {
 
         if (err) {
@@ -162,7 +141,6 @@ async function start() {
 
     });
 
-    // console.log(teamHTML);
 }
 
 
